759efa3371897c9d9f5700f869342130a1b37181
Eduardo <edulegui01@gmail.com>
2026-02-13 15:22:29 -0300

    feat: se agrego como lenvatar el servidor

---
+++ open-kiosk.bat
@@ -0,0 +1,5 @@
+@echo off
+title FE-SCO Kiosk Browser
+
+:: Abrir Chrome en modo kiosco
+start chrome --kiosk http://localhost:5173
--- start-kiosk.bat
+++ start-kiosk.bat
@@ -5,14 +5,10 @@ title FE-SCO Kiosk
 cd /d C:\fe-sco
 
 :: Levantar servidor de produccion en segundo plano
-start /B npx serve dist -s -l 3000
+start /B npx serve dist -s -l 5173
 
 :: Esperar a que el servidor levante
 timeout /t 3 >nul
 
-:: Abrir Edge en modo app + pantalla completa
-start msedge --app=http://localhost:3000 --start-fullscreen
 
 
-
-start chrome --kiosk http://localhost:3000
